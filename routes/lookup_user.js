9390cdb6acdc3f2edee2a69a178b2ea054a42852
Esalmo <Eliassalmo1234@gmail.com>
2020-05-14 02:47:44 -0700

    added lookup user

--- routes/lookup_user.js
+++ routes/lookup_user.js
@@ -32,7 +32,7 @@ router.use(bodyParser.json())
  */ 
 router.get("/:Username?", (request, response) => {
 
-    const theQuery = 'SELECT FirstName, LastName, Username FROM Members WHERE Username LIKE Esalmo'
+    const theQuery = 'SELECT FirstName, LastName, Username FROM Members WHERE Username LIKE \'Esalmo\''
     let values = [request.params.name]
 
     pool.query(theQuery, values)
