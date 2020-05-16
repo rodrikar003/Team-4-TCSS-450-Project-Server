daa057ceff049d101ae138ffdc0121f45bd27cb2
Esalmo <Eliassalmo1234@gmail.com>
2020-05-16 04:03:11 -0700

    added addContacts end point

--- routes/add_contact.js
+++ routes/add_contact.js
@@ -62,11 +62,11 @@ router.post("/", (request, response) => {
                     })
                 }
             }) 
+                } else {
+                    response.status(400).send({
+                        message: "Friend Already Exisits"
                 }
-                response.status(201).send({
-                    success: true,
-                    message: "Inserted!!!: " + result.rows[0]
-                })
+                
             })
             .catch(err => {
                 //log the error
