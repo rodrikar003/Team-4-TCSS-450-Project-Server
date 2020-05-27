61b60812041f303e5df349b06c435b9cf49ebcdf
Jake M <jacobmarquardt1@gmail.com>
2020-05-26 22:04:13 -0700

    possibly fixed problem with removechat

--- routes/chats.js
+++ routes/chats.js
@@ -509,11 +509,7 @@ router.delete("/:chatId", (request, response, next) => {
                     message: "Chat ID not found"
                 })
             } else {
-                console.log(result);
-                console.log(result.rows);
-                console.log(result.rows.memberid);
-                console.log(request.decoded.memberid);
-                if (result.rows.memberid != request.decoded.memberid) {
+                if (result.rows[0].memberid != request.decoded.memberid) {
                     response.status(404).send({
                         message: "User is not owner of chat room"
                     })
